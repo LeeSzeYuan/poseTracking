3c8d46c9754faf64a88d7e0ecd1f6fd5c107d6a0
LeeSzeYuan <55387911+LeeSzeYuan@users.noreply.github.com>
2020-09-11 10:26:21 +0800

    Add files via upload
    First Form: Nose Tracking

---
+++ pose.js
@@ -0,0 +1,35 @@
+//expect image as input
+//array of 16 coordinates with probability
+
+let video
+let poseNet
+let pose;
+
+function modelLoaded() {
+    console.log('poseNet ready')
+}
+
+function setup(){
+    createCanvas(640, 480)
+    video = createCapture(VIDEO)
+    video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function gotPoses(poses){
+    console.log(poses)
+    if(poses.length > 0){
+        pose = poses[0].pose;
+    }
+}
+
+function draw(){
+    image(video, 0, 0)
+
+    if (pose) {
+        fill(255, 0, 0)
+        ellipse(pose.nose.x, pose.nose.y, 64)
+    }
+    
+}

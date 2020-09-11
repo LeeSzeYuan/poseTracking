72b99dd7af52a6505384b0778aae76c0aac645dc
LeeSzeYuan <55387911+LeeSzeYuan@users.noreply.github.com>
2020-09-11 10:39:11 +0800

    Update pose.js
    track wrist and eye distance

--- pose.js
+++ pose.js
@@ -28,8 +28,15 @@ function draw(){
     image(video, 0, 0)
 
     if (pose) {
+        let eyeR = pose.rightEye
+        let eyeL = pose.leftEye
+        let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y)
+
         fill(255, 0, 0)
-        ellipse(pose.nose.x, pose.nose.y, 64)
+        ellipse(pose.nose.x, pose.nose.y, d/2)
+        fill(0,0, 255)
+        ellipse(pose.leftWrist.x, pose.leftWrist.y, 32)
+        ellipse(pose.rightWrist.x, pose.rightWrist.y, 32)
     }
     
 }
